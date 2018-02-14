fd9da60a1db1ee7b9f0133734d9b93b0bbe2acfe
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-02-13 18:56:10 -0700

    Added client css and js files

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/main.css', function(req, res) {
+  res.sendFile (__dirname + '/main.css');
+});
+
+app.get('/client.js', function (req, res) {
+  res.sendFile (__dirname + '/client.js');
 });
 
 io.on('connection', function(socket){
