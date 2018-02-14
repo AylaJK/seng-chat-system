6cf7418d2339ffa95f17ae802e8cdab0f7c30bee
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-02-13 19:09:56 -0700

    Switched to using path for file

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let path = require('path');
 let port = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/main.css', function(req, res) {
-  res.sendFile (__dirname + '/main.css');
+  res.sendFile(path.join(__dirname, 'main.css'));
 });
 
-app.get('/client.js', function (req, res) {
-  res.sendFile (__dirname + '/client.js');
+app.get('/client.js', function(req, res) {
+  res.sendFile(path.join(__dirname, '/client.js'));
 });
 
 io.on('connection', function(socket){
