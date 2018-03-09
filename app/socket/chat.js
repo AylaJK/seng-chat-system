00f2e2d0f5475a397b277a8b9e16f6ee4824cc7f
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-03-09 15:57:30 -0700

    Refactoring Variable Names

--- app/socket/chat.js
+++ app/socket/chat.js
@@ -10,53 +10,53 @@ let genUser = function() {
   return { id, name, colour };
 };
 
-let history = [];
+let aHistory = [];
 let addToHistory = function(record) {
-  while (history.length >= 200) {
-    history.shift(); // pop fist array element
+  while (aHistory.length >= 200) {
+    aHistory.shift(); // pop fist array element
   }
-  history.push(record); // push onto end of array
+  aHistory.push(record); // push onto end of array
 };
 let changeHistoricUsername = function (user) {
-  for (let record of history) {
+  for (let record of aHistory) {
     if (record.user.id === user.id) record.user = user;
   }
 };
 
-let users = {};
-let onlineUsers = [];
+let oUsers = {};
+let aOnlineUsers = [];
 let userSetOnline = function(user) {
-  for (let onlineUser of onlineUsers) {
-    if (onlineUser === user.id) return;
+  for (let iOnlineUserId of aOnlineUsers) {
+    if (iOnlineUserId === user.id) return;
   }
-  onlineUsers.push(user.id);
+  aOnlineUsers.push(user.id);
 };
 let userSetOffline = function(user) {
-  let i = onlineUsers.indexOf(user.id);
-  if (i >= 0) onlineUsers.splice(i, 1);
+  let i = aOnlineUsers.indexOf(user.id);
+  if (i >= 0) aOnlineUsers.splice(i, 1);
 };
 let getOnlineUsers = function () {
-  let usersOnline = {};
-  for (let onlineUser of onlineUsers) {
-    usersOnline[onlineUser] = users[onlineUser];
+  let oOnlineUsers = {};
+  for (let iOnlineUserId of aOnlineUsers) {
+    oOnlineUsers[iOnlineUserId] = oUsers[iOnlineUserId];
   }
-  return usersOnline;
+  return oOnlineUsers;
 };
 
 let init = function(io) {
   io.on('connection', function(socket){
     if (!socket.request.session.user) {
-      let newuser = genUser();
-      users[newuser.id] = newuser;
-      socket.request.session.user = newuser;
+      let oNewUser = genUser();
+      oUsers[oNewUser.id] = oNewUser;
+      socket.request.session.user = oNewUser;
       socket.request.session.save();
     }
     userSetOnline(socket.request.session.user);
     // Tell client their user info and send history
     socket.emit('hello', { 
       you: socket.request.session.user,
-      users: getOnlineUsers(),
-      history: history,
+      oUsers: getOnlineUsers(),
+      history: aHistory,
     });
     // Then tell everyone else who the new person is
     socket.broadcast.emit('user join', { user: socket.request.session.user });
@@ -79,20 +79,20 @@ let init = function(io) {
     });
 
     // User Changes Name
-    socket.on('change name', function(newname) {
-      newname = newname.trim(); // remove leading and trailing whitespace
-      if (socket.request.session.user.name === newname)
+    socket.on('change name', function(sNewName) {
+      sNewName = sNewName.trim(); // remove leading and trailing whitespace
+      if (socket.request.session.user.name === sNewName)
         return socket.emit('change name', { success: false, message: 'Cannot change to your current username' });
-      if (newname === '')
+      if (sNewName === '')
         return socket.emit('change name', { success: false, message: 'Usernames must contain visible characters' });
-      if (/^User[0-9]*$/g.test(newname))
+      if (/^User[0-9]*$/g.test(sNewName))
         return socket.emit('change name', { success: false, message: 'That username is reserved, please pick another one' });
-      for (let user of users) {
-        if (user.name === newname)
+      for (let user of oUsers) {
+        if (user.name === sNewName)
           return socket.emit('change name', { success: false, message: 'That username is already taken, please pick another one' });
       }
-      users[socket.request.session.user.id].name = newname;
-      socket.request.session.user.name = newname;
+      oUsers[socket.request.session.user.id].name = sNewName;
+      socket.request.session.user.name = sNewName;
       socket.request.session.save();
       changeHistoricUsername(socket.request.session.user);
       socket.broadcast.emit('user change name', { user: socket.request.session.user });
@@ -100,14 +100,14 @@ let init = function(io) {
     });
 
     // User Changes Colour
-    socket.on('change colour', function(newcolour) {
-      newcolour = newcolour.toLower();
-      if (newcolour.length !== 6)
+    socket.on('change colour', function(sNewColour) {
+      sNewColour = sNewColour.toLower().trim;
+      if (sNewColour.length !== 6)
         return socket.emit('change colour', { success: false, message: 'Colours must be 6 characters long' });
-      if (/[^0-9a-f]/g.test(newcolour))
+      if (/[^0-9a-f]/g.test(sNewColour))
         return socket.emit('change colour', { success: false, message: 'Colours can only contain the characters 0-9, A-F, or a-f' });
-      users[socket.request.session.user.id].colour = newcolour;
-      socket.request.session.user.colour = newcolour;
+      oUsers[socket.request.session.user.id].colour = sNewColour;
+      socket.request.session.user.colour = sNewColour;
       socket.request.session.save();
       changeHistoricUsername(socket.request.session.user);
       socket.broadcast.emit('user change colour', { user: socket.request.session.user });
