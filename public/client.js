eba56bc0d015336c4dd1c85d82cc7132517f9e45
Kayleigh Kernaghan <26681805+AylaJK@users.noreply.github.com>
2018-03-09 16:19:53 -0700

    Messages are bing sent and recieved

--- public/client.js
+++ public/client.js
@@ -5,8 +5,13 @@ $(function () {
     $('#m').val('');
     return false;
   });
-  socket.on('new message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+
+  socket.on('hello', function(info) {
+    $('#messages').append($('<li>').text('Hello ' + info.you.name + '!'));
+  });
+
+  socket.on('new message', function(record) {
+    $('#messages').append($('<li>').text(record.time + ' ' + record.user.name + ': ' + record.msg));
     window.scrollTo(0, document.body.scrollHeight);
   });
 });
